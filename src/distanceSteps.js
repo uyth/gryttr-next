9f3d116fe47881c7ca8c22ed2ae260dc5b070bb8
uy <uyth95@gmail.com>
2020-06-16 15:37:30 +0200

    Reduce number of choices for distance filtering

--- src/distanceSteps.js
+++ src/distanceSteps.js
@@ -2,7 +2,5 @@ export const distanceSteps = {
   1: { label: "50m", distanceInKm: 0.050 },
   2: { label: "100m", distanceInKm: 0.100 },
   3: { label: "250m", distanceInKm: 0.250 },
-  4: { label: "500m", distanceInKm: 0.500 },
-  5: { label: "1km", distanceInKm: 1.0 },
   6: { label: "Alle", distanceInKm: 100000 },
 }
