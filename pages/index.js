07e8a8756d0f817bddafd0710448de4fdbb29732
uy <uyth95@gmail.com>
2020-06-17 00:14:04 +0200

    Add links on front page for easier access

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { Container } from '@material-ui/core/';
+import { Button, Space } from "antd";
+
 export default function Home() {
   return (
-    <div>
-      HJEM
-    </div>
+    <Container>
+      <Space>
+        <Button type="main" href="/map">Klikk her for å gå til kartvisning</Button>
+        <Button type="main" href="/results">Klikk her for å gå til listevisning</Button>
+      </Space>
+    </Container>
   )
 }
