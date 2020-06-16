8d336b4bf0bcbea840835e5e4fe9bbd6a3495032
uy <uyth95@gmail.com>
2020-06-16 18:42:50 +0200

    Update distances used in FilterDrawer

--- components/FilterDrawer/DistanceSlider.js
+++ components/FilterDrawer/DistanceSlider.js
@@ -1,16 +1,8 @@
 import React, { useContext, useState } from 'react';
 import { store } from '../../src/store.js';
+import { distanceSteps } from '../../src/distanceSteps';
 
 import { Slider } from 'antd';
-
-const distances = {
-  1: { label: "50m", value: 0.050 },
-  2: { label: "100m", value: 0.100 },
-  3: { label: "250m", value: 0.250 },
-  4: { label: "500m", value: 0.500 },
-  5: { label: "1km", value: 1.0 },
-  6: { label: "Alle", value: 100000 },
-}
 
 export default function DistanceSlider() {
 
@@ -33,9 +25,9 @@ export default function DistanceSlider() {
       value={distanceRadiusStep}
       onChange={handleDistanceRadiusStepChange}
       onAfterChange={commitDistanceRadiusStep}
-      marks={distances}
+      marks={distanceSteps}
       min={1}
-      max={Object.keys(distances).length}
+      max={Object.keys(distanceSteps).length}
       step={1}
       tipFormatter={(value) => distances[value].label}
     />
