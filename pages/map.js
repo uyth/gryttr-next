9f0cefccb35ef64c43f4067642a9ff66e0ce6e24
uy <uyth95@gmail.com>
2020-06-17 19:40:16 +0200

    Wait for leaflet css to load

--- pages/map.js
+++ pages/map.js
@@ -99,7 +99,6 @@ export default function MapView() {
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
           integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
           crossorigin=""
-          media="print" onload="this.media='all'"
         />
         <link
           rel="stylesheet"
