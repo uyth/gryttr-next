b82538ed33a4c4a34999cc0f673138ba10f6c6f4
uy <uyth95@gmail.com>
2020-06-17 17:58:04 +0200

    Load leaflet async

--- pages/map.js
+++ pages/map.js
@@ -92,14 +92,18 @@ export default function MapView() {
       <Head>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
           integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-          crossorigin="" />
+          crossorigin=""
+          media="print" onload="this.media='all'"
+        />
+        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+          integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+          crossorigin=""
+          async
+        />
         <link
           rel="stylesheet"
           href="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css"
         />
-        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-          integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-          crossorigin="" />
         <script src="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css"></script>
       </Head>
       <DynamicComponentWithNoSSR boulders={boulders} />
