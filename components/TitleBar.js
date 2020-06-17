a1adbf9bad76168df2deae74136a270a6fc13ba2
uy <uyth95@gmail.com>
2020-06-17 16:50:39 +0200

    Add alt to image

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -6,7 +6,7 @@ import { Container } from '@material-ui/core/';
 export default function TitleBar() {
   return (
     <Container>
-      <img src="/logo.svg" height="20em"/>
+      <img src="/logo.svg" height="20em" alt="Logo"/>
     </Container>
   )
 };
