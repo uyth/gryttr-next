81890f7921e1998e71e16bab30dc5f7fc524ff06
uy <uyth95@gmail.com>
2020-06-17 19:39:22 +0200

    Remove leaflet script

--- pages/map.js
+++ pages/map.js
@@ -101,11 +101,6 @@ export default function MapView() {
           crossorigin=""
           media="print" onload="this.media='all'"
         />
-        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-          integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-          crossorigin=""
-          async
-        />
         <link
           rel="stylesheet"
           href="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css"
